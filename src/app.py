c7fce1e1456acc566c68ff47ff6b67b873a57776
Justin <justin.schmidt@student.fhws.de>
2022-04-15 15:36:30 +0200

    First Login page defined.

--- src/app.py
+++ src/app.py
@@ -1,10 +1,23 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template
+from flask import Flask, render_template, Blueprint
+#from . import db
+
 
 #Create a Flask Instance
 app = Flask(__name__)
+main = Blueprint('main', __name__)
+
 
 #Create a route decorator
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+
+app.run(debug=True)
