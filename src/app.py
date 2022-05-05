dd575d6bd191befa4109a26401f8a024d703a83a
Meinhardt, Hannah <hannah.meinhardt@student.fhws.de>
2022-05-05 13:35:57 +0200

    Logout Teil 1

--- src/app.py
+++ src/app.py
@@ -1,21 +1,31 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, Blueprint
+from flask import Flask, render_template, Blueprint, session
 #from . import db
 
 #Create a Flask Instance
 app = Flask(__name__)
 main = Blueprint('main', __name__)
+app.secret_key = "secret key"
 
 
 #Create a route decorator
 @app.route("/")
 def index():
+    # session['logged_in'] = False erst wenn der Button geklickt wird (Funktion noch nicht implementiert)
+    session['logged_in'] = False
     return render_template("index.html")
 
 
 @app.route("/login")
 def login():
+    session['logged_in'] = True
     return render_template("login.html")
+
+
+@app.route('/logout')
+def logout():
+    session['logged_in'] = False
+    return render_template("profile.html")
 
 @app.route("/profile", methods = ['POST'])
 def profile():
--- src/auth.py
+++ src/auth.py
@@ -1,10 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
 #from . import db
 
 auth = Blueprint('auth', __name__)
 
 @auth.route("/login")
 def login():
+    # session['logged_in'] = False erst wenn der Button geklickt wird (Funktion noch nicht implementiert)
+    session['logged_in'] = True
     return render_template("login.html")
 
 
@@ -12,6 +14,7 @@ def login():
 #def signup():
  #   return 'Signup'
 
-#@auth.route('/logout')
-#def logout():
-  #  return 'Logout'
+@auth.route('/logout')
+def logout():
+    session['logged_in'] = False
+    return render_template("profile.html")
